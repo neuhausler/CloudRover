2abf0457f0661a6674f2121863c4f1fb2742955b
Marcel Neuhausler <neuhausler@gmail.com>
2012-01-08 23:23:10 -0500

    added path for erl

--- start-service.sh
+++ start-service.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin
 cd `dirname $0`
 exec erl -noshell -detached -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s cloudrover
