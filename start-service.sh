93146af95cbf2ecb14cc253aba0d260c79cda5f8
Marcel Neuhausler <neuhausler@gmail.com>
2012-01-29 12:50:02 -0500

    Removed start-service script .. moved it into ec2cloudrover

--- start-service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-PATH=/sbin:/usr/sbin:/bin:/usr/bin:/usr/local/bin:/usr/local/sbin
-cd `dirname $0`
-run_erl -daemon priv/var/ priv/log/ "exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s cloudrover"
